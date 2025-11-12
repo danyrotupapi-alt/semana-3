39559f3b1f2229fa76bb533bcaeb0bec8588ec5f
daniel <danyrotupapi@gmail.com>
2025-11-11 22:36:22 -0500

    laboratorio 3

---
+++ semana 3/semana 3/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Números pares del 1 al 20:");
+
+        for (int i = 1; i <= 20; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        // Mantener la consola abierta
+        Console.WriteLine("Presiona cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
